932bcf0aea8397eed72fad180278850a6d870456
Pereira Djullian <djullianpereira@gmail.com>
2025-04-06 18:54:15 -0300

    front com angular19 e bootstrap para a aplicacao de board de tarefas

---
+++ BoardTarefas/src/main/java/br/com/dio/BoardTarefasApplication.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTarefasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoardTarefasApplication.class, args);
+    }
+}
---
+++ BoardTarefas/src/main/java/br/com/dio/controller/TaskController.java
@@ -0,0 +1,2 @@
+package br.com.dio.controller;public class TaskController {
+}
